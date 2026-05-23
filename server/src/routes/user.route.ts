c720744165705a2a8ae7bacbc14078f6e74648cb
Hiroki Inoue <tyynh.hiroki@gmail.com>
2026-05-22 18:40:47 -0700

    feat / protect fetch users info with auth middleware

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,10 +1,11 @@
 import { Router, Request, Response } from "express";
 import userController from "../controllers/user.controller";
+import { authMiddleware } from "../middleware/auth.middleware";
 
 const userRouter = Router();
 
-userRouter.get("/", userController.getAllUsers);
-userRouter.get("/:id", userController.getUserById);
+userRouter.get("/", authMiddleware, userController.getAllUsers);
+userRouter.get("/:id", authMiddleware, userController.getUserById);
 userRouter.post("/signup", userController.addUser);
 userRouter.post("/login", userController.login);
 userRouter.post("/refresh", userController.restoreAccessToken);
