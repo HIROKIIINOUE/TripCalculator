c07ea7e234117b7df2da62b285d385ab899eb6cf
Hiroki Inoue <tyynh.hiroki@gmail.com>
2026-05-26 11:45:10 -0700

    feat / show footer on mobile but header on other devices

--- client/src/layouts/PageLayout.tsx
+++ client/src/layouts/PageLayout.tsx
@@ -5,11 +5,15 @@ import { Outlet } from 'react-router'
 const PageLayout = () => {
   return (
     <div className='min-h-screen flex flex-col'>
-      <Header />
-      <main>
+      <header className='hidden sm:block'>
+        <Header />
+      </header>
+      <main className='flex-1'>
         <Outlet />
       </main>
-      <Footer />
+      <footer className='sm:hidden'>
+        <Footer />
+      </footer>
     </div>
   )
 }
