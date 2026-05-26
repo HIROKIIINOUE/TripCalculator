8326e226ccaf0b33a1a93d3c56b39ab00b67d756
Hiroki Inoue <tyynh.hiroki@gmail.com>
2026-05-26 12:30:52 -0700

    design: add layout design

--- client/src/layouts/PageLayout.tsx
+++ client/src/layouts/PageLayout.tsx
@@ -4,11 +4,11 @@ import { Outlet } from 'react-router'
 
 const PageLayout = () => {
   return (
-    <div className='min-h-screen flex flex-col'>
+    <div className='min-h-screen bg-gradient-to-br from-orange-100 via-orange-200 to-orange-100 text-slate-800'>
       <header className='hidden sm:block'>
         <Header />
       </header>
-      <main className='flex-1'>
+      <main className='mx-auto flex w-full max-w-7xl flex-1 flex-col px-4 pb-24 pt-6 sm:px-6 sm:pb-10 sm:pt-24 lg:px-8'>
         <Outlet />
       </main>
       <footer className='sm:hidden'>
