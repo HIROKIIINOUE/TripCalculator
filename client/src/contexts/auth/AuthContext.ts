410b0f2a11ed51dafab92d268222cd0b5ee7f408
Hiroki Inoue <tyynh.hiroki@gmail.com>
2026-05-29 13:21:22 -0700

    feat / implement current user's authentication checker

--- client/src/contexts/auth/AuthContext.ts
+++ client/src/contexts/auth/AuthContext.ts
@@ -1,12 +1,18 @@
 import { createContext } from "react";
 import type { User } from "../../types/user.type";
 
+export type AuthCheckResult =
+  | { result: "success"; data: User }
+  | { result: "failure"; data: null };
+
 type AuthContextType = {
   user: User | null;
   accessToken: string;
   authStatus: "unauthenticated" | "authenticated" | "checking";
   storeAuth: (user: User, token: string) => void;
   clearAuth: () => void;
+  checkUserAuthentication: (token: string) => Promise<AuthCheckResult>;
+  restoreAccessToken: () => void;
 };
 
 export const AuthContext = createContext<AuthContextType>({
@@ -15,4 +21,6 @@ export const AuthContext = createContext<AuthContextType>({
   authStatus: "checking",
   storeAuth: () => {},
   clearAuth: () => {},
+  checkUserAuthentication: async () => ({ result: "failure", data: null }),
+  restoreAccessToken: () => {},
 });
--- client/src/contexts/auth/AuthContextProvider.tsx
+++ client/src/contexts/auth/AuthContextProvider.tsx
@@ -1,13 +1,15 @@
 import { useState, type ReactNode } from "react"
-import { AuthContext } from "./AuthContext"
+import { AuthContext, type AuthCheckResult } from "./AuthContext"
 import type { User } from "../../types/user.type"
 
+
 export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
+  const BACKEND_URL = import.meta.env.VITE_BACKEND_URL_DEV
   const [user, setUser] = useState<User | null>(null)
   const [accessToken, setAccessToken] = useState<string>("")
   const [authStatus, setAuthStatus] = useState<"unauthenticated" | "authenticated" | "checking">("checking")
 
-  const login = (user: User, token: string) => {
+  const storeAuth = (user: User, token: string) => {
     setAccessToken(token)
     setUser(user)
     setAuthStatus("authenticated")
@@ -20,9 +22,59 @@ export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
     setAuthStatus("unauthenticated")
   }
 
+  // check if current logged-in user is still authenticated
+  const checkUserAuthentication = async (token: string): Promise<AuthCheckResult> => {
+    setAuthStatus("checking")
+
+    const res = await fetch(`${BACKEND_URL}/users/me`, {
+      method: "GET",
+      headers: {
+        "authorization": `Bearer ${token}`
+      }
+    })
+    const data = await res.json()
+
+    if (res.status === 200) {
+      setAuthStatus("authenticated")
+      return { result: "success", data }
+    }
+    setAuthStatus("unauthenticated")
+    return { result: "failure", data: null }
+
+  }
+
+  // try to restore access token from current logged-in user's refresh token in Cookie
+  const restoreAccessToken = async () => {
+    setAuthStatus("checking")
+
+    try {
+      const res = await fetch(`${BACKEND_URL}/users/refresh`, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json"
+        },
+        credentials: "include",
+      })
+      if (res.status !== 200) {
+        clearAuth()
+        return
+      }
+      const data = await res.json()
+      const user = await checkUserAuthentication(data.accessToken)
+      if (user.result === "success") {
+        storeAuth(user.data, data.accessToken)
+      } else {
+        clearAuth()
+      }
+    } catch (error) {
+      console.error(error)
+      clearAuth()
+    }
+  }
+
   return (
 
-    <AuthContext.Provider value={{ user, accessToken, authStatus, login, clearAuth }}>
+    <AuthContext.Provider value={{ user, accessToken, authStatus, storeAuth, clearAuth, checkUserAuthentication, restoreAccessToken }}>
       {children}
     </AuthContext.Provider>
   )
