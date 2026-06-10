210c6b942f9d5a8108e52d1ca03764ef15de794a
Hiroki Inoue <tyynh.hiroki@gmail.com>
2026-06-09 19:26:52 -0700

    error : make detail data in event optional

--- server/src/schemas/event.schema.ts
+++ server/src/schemas/event.schema.ts
@@ -5,7 +5,7 @@ const MAX_EVENT_PRICE = 2147483647;
 export const createEventSchema = z.object({
   date: z.iso.date(),
   title: z.string().min(1),
-  detail: z.string().min(1),
+  detail: z.string(),
   localCurrency: z.string().min(1),
   priceLocalCurrency: z.number().int().min(0).max(MAX_EVENT_PRICE),
 });
@@ -15,7 +15,7 @@ export type CreateEventBody = z.infer<typeof createEventSchema>;
 export const updateEventSchema = z.object({
   date: z.iso.date().optional(),
   title: z.string().min(1).optional(),
-  detail: z.string().min(1).optional(),
+  detail: z.string().optional(),
   localCurrency: z.string().min(1).optional(),
   priceLocalCurrency: z.number().int().min(0).max(MAX_EVENT_PRICE).optional(),
   priceYourCurrency: z.number().int().optional(),
